8fd7fb3523565780e453cac51da37d1490515951
smrsln <oasimarslan@gmail.com>
2022-05-06 21:02:45 +0300

    JSM memories MERN from YouTube

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Post from "./Post/Post";
+import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
+import useStyles from "./styles";
+const Posts = () => {
+  const classes = useStyles();
+  const posts = useSelector((store) => store.posts);
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default Posts;
--- server/index.js
+++ server/index.js
@@ -6,12 +6,11 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/posts", postRoutes);
 const CONNECTION_URL =
   "mongodb+srv://javascriptmastery:javascriptmastery123@memoriesdbcluster.p2ml6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5432;
